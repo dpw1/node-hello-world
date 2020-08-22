29f80ed365bdc653f742b309b1ade95233a8910a
dpw1 <diego.boarutto.fortes@gmail.com>
2020-08-22 08:03:06 -0300

    [

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ endpoint: "/root" });
-});
+// app.get("/", (req, res) => {
+//   res.json({ endpoint: "/root" });
+// });
 
-app.get("/books", (req, res) => {
-  res.json({ endpoint: "/books" });
-});
+// app.get("/books", (req, res) => {
+//   res.json({ endpoint: "/books" });
+// });
 
-app.listen(3000, () => console.log("LISTEN 3000"));
+// app.listen(3000, () => console.log("LISTEN 3000"));
+
+module.exports = (req, res) => {
+  const { name = "World" } = req.query;
+  res.status(200).send(`Hello ${name}!`);
+};
