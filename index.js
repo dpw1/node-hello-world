72fbbb7d3d458b235dd91bbb01502db4ac90cea3
dpw1 <diego.boarutto.fortes@gmail.com>
2020-08-22 06:05:29 -0300

    upd

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var express = require("express");
-var app = express();
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
-app.get("/url", (req, res, next) => {
-  res.json(["Tony", "Lisa", "Michael", "Ginger", "Foodsss"]);
-});
+module.exports = (req, res) => {
+  const { name = "World" } = req.query;
+  res.status(200).send(`Hello ${name}!`);
+};
